dbc624f745eff986277752b6ae4c6d28e02c1cfd
Mircea Stan <mircea.stan@masabi.com>
2021-10-12 10:42:13 +0300

    rename default tenant

--- src/main/java/com/mirsilstan/multitenant/TenantContext.java
+++ src/main/java/com/mirsilstan/multitenant/TenantContext.java
@@ -2,17 +2,13 @@ package com.mirsilstan.multitenant;
 
 public class TenantContext {
     private static ThreadLocal<String> currentTenant = new InheritableThreadLocal<>();
-
-    public static final String DEFAULT_TENANT_ID = "Broker_Master";
-
+    public static final String DEFAULT_TENANT_ID = "default_tenant";
     public static String getCurrentTenant() {
         return currentTenant.get();
     }
-
     public static void setCurrentTenant(String tenant) {
         currentTenant.set(tenant);
     }
-
     public static void clear() {
         currentTenant.set(null);
     }
